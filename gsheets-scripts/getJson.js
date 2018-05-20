0556decbf6d9290733236f298a826ce13047914c
Alexander Chaschin <chaschin@uchi.ru>
2018-05-20 10:58:44 +0300

    from strings to json object stringification

--- gsheets-scripts/getJson.js
+++ gsheets-scripts/getJson.js
@@ -1,7 +1,3 @@
-// TODO
-// #NA or empty audio paths
-// styling
-
 // Creates a custom menu in Google Sheets when the spreadsheet opens.
 function onOpen() {
   SpreadsheetApp.getUi()
@@ -10,46 +6,120 @@ function onOpen() {
     .addToUi();
 }
 
-// prettier-ignore
-function getStyles_() {
-  var result =
-        '<style>'
-      + '.json-key { color: brown; }'
-      + '.json-value { color: navy; }'
-      + '.json-string { color: olive; }'
-      + '</style>';
-  return result;
-}
-
 // accessible from 'Make JSON const entries' menu
 function makeJsonSidebar_() {
-  var htmlOutput = HtmlService
-    //.createHtmlOutput(getStyles_() + '<pre>' + jsonPrettyPrint.process(makeJson_()) + '</pre>')
-    .createHtmlOutput(getStyles_() + '<pre>' + makeJson_() + '</pre>')
-    .setTitle('JSON data');
+  Logger.log(makeJson_());
+  var htmlOutput = HtmlService.createHtmlOutput(getSidebarContent_()).setTitle(
+    'Chunk json const entries'
+  );
   SpreadsheetApp.getUi().showSidebar(htmlOutput);
 }
 
-// assert current selected range
+// this function is used in sidebar to copy json to clipboard
+function getCopyScript_() {
+  var dummy = document.createElement('textarea');
+  document.body.appendChild(dummy);
+  dummy.setAttribute('id', 'dummy_id');
+  document.getElementById('dummy_id').value = unescapeHtml_(
+    document.getElementById('jsonData').innerHTML
+  );
+  dummy.select();
+  document.execCommand('copy');
+  document.body.removeChild(dummy);
+  document.getElementById('copy-button').innerHTML = 'Copied!';
+}
+
+function getSidebarContent_() {
+  return (
+    '' +
+    '<script>' +
+    unescapeHtml_.toString() +
+    '</script>' +
+    '<script>' +
+    getCopyScript_.toString() +
+    '</script>' +
+    '<button onclick="getCopyScript_()" id="copy-button">Copy to clipboard</button>' +
+    '<pre id="jsonData">' +
+    makeJson_() +
+    '</pre>'
+  );
+}
+
+// assert current selected range and makes json string from it
 function makeJson_() {
   var rangeValues = SpreadsheetApp.getActiveSheet()
     .getActiveRange()
     .getValues();
   assertRange_(rangeValues);
-  var jsonData = toJson_(rangeValues);
-  return jsonData;
+  return rangeValues.map(jsonStringFromRow_).join(',\n');
 }
 
 // assert
 function assertRange_(range) {
-  var isError = !range || range.length < 1 || range[0].length < 5;
-  if (isError)
+  var isErrorInRange = !range || range.length < 1 || range[0].length < 5;
+  if (isErrorInRange)
     throw new Error('Please select range of cells with 5 columns or more');
+}
+
+// paths to audio files in repository for different languages
+var audioPaths = {
+  en: {
+    mp3: '/share/happy_numbers/assets/sound/all/English-All/Mp3_128kbps/',
+    ogg: '/share/happy_numbers/assets/sound/all/English-All/Ogg/'
+  },
+  es: {
+    mp3: '/share/happy_numbers/assets/sound/all/Spanish-All/Mp3_128kbps/',
+    ogg: '/share/happy_numbers/assets/sound/all/Spanish-All/Ogg/'
+  }
+};
+
+// text/audio entry
+function makeLangEntry_(lang, text, audioFilename) {
+  var currentPaths = audioPaths[lang];
+  if (!currentPaths) throw new Error('Cant find paths for language: ' + lang);
+  var r = {
+    text: text,
+    audio: {
+      mp3: currentPaths.mp3 + audioFilename + '.mp3',
+      ogg: currentPaths.ogg + audioFilename + '.ogg'
+    }
+  };
+  return r;
 }
 
 // audio file name without extension
 function voiceName_(filename) {
   return filename.split('.')[0];
+}
+
+// row is 5-item array
+function jsonFromRow_(row) {
+  var textEng = escapeHtml_(row[0]);
+  var textEsp = escapeHtml_(row[1]);
+  var audioEng = voiceName_(row[2]);
+  var audioEsp = voiceName_(row[3]);
+  var jsonKey = escapeHtml_(row[4]);
+
+  var json = {};
+  json[jsonKey] = {
+    base: { speaker_say: 'every_time' },
+    en: makeLangEntry_('en', textEng, audioEng),
+    es: makeLangEntry_('es', textEsp, audioEsp)
+  };
+  return json;
+}
+
+// convert json to string
+// and removing leading and ending curly brackets,
+// and removing two leading spaces in each line
+function jsonStringFromRow_(row) {
+  return JSON.stringify(jsonFromRow_(row), null, 2)
+    .split('\n')
+    .slice(1, -1)
+    .map(function(str) {
+      return str.replace(/\s{2}/, '').replace(/\\\\/g, '\\');
+    })
+    .join('\n');
 }
 
 // copyright by mustache.js
@@ -69,106 +139,22 @@ function escapeHtml_(string) {
   });
 }
 
-// speakerEntry
-// prettier-ignore
-function makeSpeakerEntry_() {
-  var result =
-        '  "base": {\n'
-      + '    "speaker_say": "every_time"\n'
-      + '  }';
-  return result;
-}
-
-// audio entry for specified extension
-// prettier-ignore
-function makeAudioEntry_(extension, pathPrefix, filename) {
-  var result =
-        '      "' + extension + '":\n'
-      + '        "' + pathPrefix + filename + '.' + extension + '"';
-  return result;
-}
-
-// text/audio entry
-// prettier-ignore
-function makeLangEntry_(lang, text, audioFilename) {
-  var paths = {
-    'en': {
-      mp3: '/share/happy_numbers/assets/sound/all/English-All/Mp3_128kbps/',
-      ogg: '/share/happy_numbers/assets/sound/all/English-All/Ogg/'
-    },
-    'es': {
-      mp3: '/share/happy_numbers/assets/sound/all/Spanish-All/Mp3_128kbps/',
-      ogg: '/share/happy_numbers/assets/sound/all/Spanish-All/Ogg/'
-    }
+// reversed by me
+function unescapeHtml_(string) {
+  var entityMap = {
+    '&amp;': '&',
+    '&lt;': '<',
+    '&gt;': '>',
+    '&quot;': '"',
+    '&#39;': "'",
+    '&#x2F;': '/',
+    '&#x60;': '`',
+    '&#x3D;': '='
   };
-  var currentPaths = paths[lang];
-  if(!currentPaths) throw new Error('Cant find paths for language ' + lang);
-
-  var pathMp3 = currentPaths.mp3;
-  var pathOgg = currentPaths.ogg;
-  var result =
-        '  "' + lang + '": {\n'
-      + '    "text":\n'
-      + '      "' + text + '",\n'
-      + '    "audio": {\n'
-      +      makeAudioEntry_('mp3', pathMp3, audioFilename) + ',\n'
-      +      makeAudioEntry_('ogg', pathOgg, audioFilename) + '\n'
-      + '    }\n'
-      + '  }';
-  return result;
+  return String(string).replace(
+    /&amp;|&lt;|&gt;|&quot;|&#39;|&#x2F;|&#x60;|&#x3D;/g,
+    function(s) {
+      return entityMap[s];
+    }
+  );
 }
-
-// row is 5-item array
-// prettier-ignore
-function rowToJson_(row) {
-  var textEng  = escapeHtml_(row[0]);
-  var textEsp  = escapeHtml_(row[1]);
-  var audioEng = voiceName_(row[2]);
-  var audioEsp = voiceName_(row[3]);
-  var jsonKey  = escapeHtml_(row[4]);
-  var result =
-        '"' + jsonKey + '": {\n'
-      + makeSpeakerEntry_() + ',\n'
-      + makeLangEntry_('en', textEng, audioEng) + ',\n'
-      + makeLangEntry_('es', textEsp, audioEsp) + '\n'
-      + '}';
-  return result;
-}
-
-// range is already asserted, it is two-dim array and each in-array length' is 5
-function toJson_(range) {
-  var result = range.map(rowToJson_).join(',\n');
-  return result;
-}
-
-// json prettifier
-// copyright by Dem Pilafian
-var jsonPrettyPrint = {
-  replacer: function(match, pIndent, pKey, pVal, pEnd) {
-    var key = '<span class=json-key>';
-    var val = '<span class=json-value>';
-    var str = '<span class=json-string>';
-    var r = pIndent || '';
-    if (pKey) r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
-    if (pVal) r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
-    return r + (pEnd || '');
-  },
-  toHtml: function(obj) {
-    var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/gm;
-    return JSON.stringify(obj, null, 3)
-      .replace(/&/g, '&amp;')
-      .replace(/\\"/g, '&quot;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(jsonLine, jsonPrettyPrint.replacer);
-  },
-  process: function(str) {
-    var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/gm;
-    return str
-      .replace(/&/g, '&amp;')
-      .replace(/\\"/g, '&quot;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(jsonLine, jsonPrettyPrint.replacer);
-  }
-};
